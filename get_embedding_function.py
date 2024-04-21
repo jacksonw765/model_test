6f28f898ac83e950684759a739437994ada3a603
Jackson MBP <jacksonw765@gmail.com>
2024-04-21 18:39:27 -0400

    removed api key

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -6,11 +6,11 @@ from langchain_community.embeddings import OpenAIEmbeddings
 def get_embedding_function():
     #embeddings = BedrockEmbeddings(
     #    credentials_profile_name="default", region_name="us-east-1"
-    #)
+    #) #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
     
     embeddings = OpenAIEmbeddings(
         
-        openai_api_key="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"
+        openai_api_key=""
     )
     #embeddings = OllamaEmbeddings(model="nomic-embed-text") #
     return embeddings
