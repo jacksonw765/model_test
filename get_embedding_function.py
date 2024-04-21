807c54fb42fb103b971c5dd53f5de5505d1006f0
Jackson MBP <jacksonw765@gmail.com>
2024-04-21 18:39:59 -0400

    removed api key

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -6,7 +6,7 @@ from langchain_community.embeddings import OpenAIEmbeddings
 def get_embedding_function():
     #embeddings = BedrockEmbeddings(
     #    credentials_profile_name="default", region_name="us-east-1"
-    #) #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
+    #) 
     
     embeddings = OpenAIEmbeddings(
         
